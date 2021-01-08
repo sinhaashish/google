2c39f91a556219459bfed3ebeaa8c03a6ba860b6
sinhaashish <ashi.sinha.87@gmail.com>
2021-01-08 22:13:52 +0530

    Print the LCS

---
+++ src/dynamicprogramming/PrintTheLCS.java
@@ -0,0 +1,65 @@
+package dynamicprogramming;
+
+public class PrintTheLCS {
+
+  public static void main(String[] args) {
+    String a = "abcplm";
+    String b = "adcpme";
+
+    System.out.println(" The LCS  of " + a + " and  " + b + " is " + LCS(a, b));
+
+  }
+
+
+  static String LCS(String a, String b) {
+
+    int m = a.length();
+    int n = b.length();
+    int dp[][] = new int[m + 1][n + 1];
+    //Initialize first row and column to 0
+    for (int i = 0; i < m + 1; i++) {
+      for (int j = 0; j < n + 1; j++) {
+        if (i == 0 || j == 0) {
+          dp[i][j] = 0;
+        }
+      }
+    }
+    for (int i = 1; i < m + 1; i++) {
+      for (int j = 1; j < n + 1; j++) {
+        if (a.charAt(i - 1) == b.charAt(j - 1)) {
+          dp[i][j] = 1 + dp[i - 1][j - 1];
+        } else {
+          dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+        }
+      }
+    }
+
+    for ( int i = 0 ; i < dp.length; i ++) {
+      for (int j = 0; j < dp[0].length; j++) {
+        System.out.print(" " + dp[i][j]);
+      }
+      System.out.println();
+    }
+
+    StringBuilder s = new StringBuilder();
+
+
+    int i = m; int j= n;
+    while (i > 0 || j> 0) {
+      if (a.charAt(i-1) == b.charAt(j-1)) {
+        s.append(a.charAt(i-1));
+        i--;
+        j--;
+      } else {
+        if (dp[i][j - 1] > dp[i - 1][j]) {
+          j--;
+        } else {
+          i--;
+        }
+      }
+    }
+
+    return s.reverse().toString();
+  }
+
+}
